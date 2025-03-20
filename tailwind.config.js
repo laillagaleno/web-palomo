430933d6961e41b1adb39145eaa619b915f6fed4
laillagaleno <laillagalenoitz@gmail.com>
2025-03-20 10:44:37 -0300

    responsividade

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    content: ["./*.html"], // Ajuste conforme o caminho dos seus arquivos HTML
+    theme: {
+      extend: {
+        fontFamily: {
+          red: ['"Red Hat Display"', 'sans-serif'], // Nome da fonte + fallback
+        },
+      },
+    },
+    plugins: [],
+  };
+  
